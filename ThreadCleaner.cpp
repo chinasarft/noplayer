b000ee6376ea12a5604586c73f81c07227740f26
lmk <963547817@qq.com>
2018-07-04 09:35:57 +0800

    add StopClass

--- ThreadCleaner.cpp
+++ ThreadCleaner.cpp
@@ -5,12 +5,27 @@
 ThreadCleaner::ThreadCleaner()
 {
     auto clean = [this](){
-        while(shouldQuit_ == false && threads_.size() == 0) {
-            std::thread t;
-            t.swap(threads_.front());
-            threads_.pop_front();
-            if (t.joinable()) {
-                t.join();
+        while(shouldQuit_ == false) {
+            bool hasItem = false;
+            if (threads_.size() > 0) {
+                hasItem = true;
+                std::thread t;
+                t.swap(threads_.front());
+                threads_.pop_front();
+                if (t.joinable()) {
+                    t.join();
+                }
+            }
+
+            if (objsToStop_.size() > 0) {
+                hasItem = true;
+                std::shared_ptr<StopClass> c = objsToStop_.front();
+                threads_.pop_front();
+                c->Stop();
+            }
+
+            if (hasItem == false) {
+                condition_.wait();
             }
         }
     };
@@ -21,6 +36,7 @@ ThreadCleaner::ThreadCleaner()
 void ThreadCleaner::Stop()
 {
     shouldQuit_ = true;
+    condition_.notify_one();
     if (cleaner_.joinable()) {
         cleaner_.join();
     }
@@ -39,6 +55,16 @@ void ThreadCleaner::Push(std::thread && t)
     }
     std::unique_lock<std::mutex> lock(mutex_);
     threads_.emplace_back(std::forward<std::thread>(t));
+    condition_.notify_one();
+}
+
+void ThreadCleaner::Push(std::shared_ptr<StopClass> &&c)
+{
+    if (shouldQuit_) {
+        return;
+    }
+    std::unique_lock<std::mutex> lock(mutex_);
+    objsToStop_.emplace_back(c);
     condition_.notify_one();
 }
 
